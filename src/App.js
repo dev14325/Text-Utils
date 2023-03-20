3db5a27d01fb7578bf54cfdf279226e92da53649
Dev Pratap Singh <devthakur31931@gmail.com>
2023-03-20 13:46:56 +0530

    updating textUtils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import About from './components/About';
+import TextForm from './components/TextForm';
+import react , { useState } from 'react';
+
+// import About from './components/About';
 
 
 import './App.css';
 
 
 
+
 function App() {
+  const [mode,setMode] = useState('light');
+
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'black';
+    }
+  }
  
   return (
    <> 
  
-<Navbar title = "textUtils" aboutTextUtils = "about us"/>
+<Navbar title = "textUtils" mode= {mode} toggle = {toggleMode} aboutTextUtils = "About us"/>
 <div className="container my-3" >
-  {/* <TextForm heading = "Enter text below to analyze"/> */}
-  <About/>
+  <TextForm heading = "Enter text below to analyze" mode= {mode} />
+  {/* <About/> */}
 </div>
 
-{/* <Navbar/> */}
+<Navbar/>
    </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -44,23 +44,23 @@ export default function TextForm(props) {
     const [text,setText] = useState('')
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color : props.mode==='dark'?'white' : '#042743'}}>
    
+<h1 >{props.heading}</h1>
 <div className="mb-3">
-<h1>{props.heading}</h1>
-  <label htmlFor= "exampleFormControlTextarea1" className="form-label"></label>
-  <textarea className="form-control"  value = {text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"> </textarea>
-  <div className="container my-3"></div>
+  
+  <textarea className="form-control"  value = {text} onChange={handleOnChange} style={{backgroundColor : props.mode==='dark'?'grey' : 'white',color : props.mode==='dark'?'white' : '#042743'}} id="exampleFormControlTextarea1" rows="8"> </textarea>
+ </div>
   <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
   <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
   <button className="btn btn-primary mx-2" onClick={handleclearClick}>Clear</button>
   <button className="btn btn-primary mx-2" onClick={handlerevClick}>Reverse the Text</button>
-  <button className='btn btn-primary mx-2' onClick={downloadTxtFile}>Download File</button>
-</div>
+  <button className='btn btn-primary mx-2' onClick={downloadTxtFile}>Download </button>
+
       
     </div>
 
-    <div className="container my-3">
+    <div className="container my-3" style={{color : props.mode==='dark'?'white' : '#042743'}}>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} Words and {text.length} Characters</p>
         <p>{0.008 * text.split(" ").length}Minutes taken to read the text</p>
